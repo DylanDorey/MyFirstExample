4caf010d87e0eb9065e7c8e0710c3a6a999da465
Dylan Dorey <dedorey@csuchico.edu>
2025-09-10 12:20:26 -0700

    test commit

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ int main()
     std::cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
+    double output;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
+
+    //addition
+    double output = x + y;
+    std::cout << x << " + " << y << " = " << output;
+
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
     std::cout << "Division: " << x / y << std::endl;
